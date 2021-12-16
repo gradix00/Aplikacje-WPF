a90406eb47f0b606610af165d80027bf57e24acd
Adrian (adiomi games) <89781516+gradix00@users.noreply.github.com>
2021-12-16 22:51:30 +0100

    1.0.1
    added function refresh word and check letter

--- Wiesielec/MainWindow.xaml.cs
+++ Wiesielec/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace Wiesielec
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region variable
+        private string[] words = { "kabanos", "ala ma kota", "komputer jest przyszłością" };
+        private char[] currentlyPass;
+        private string pass;
+        private int mistake = 0;
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            pass = RandomWord();
+            passTxt.Content = pass;
+
+            currentlyPass = new char[pass.Length];
+            for(int i = 0; i < currentlyPass.Length; i++)
+            {
+                currentlyPass[i] = '_';
+            }
+
+            RefreshPasswordAndStickman();
+        }
+
+        private string RandomWord()
+        {
+            var rand = new Random();
+            return words[rand.Next(0, words.Length)].ToString();
+        }
+
+        private void RefreshPasswordAndStickman()
+        {
+            for(int index = 0; index < pass.Length; index++)
+            {
+                passTxt.Content = null;
+                passTxt.Content += $" {currentlyPass[index]}";
+            }
+            Console.WriteLine($"length: {pass.Length}");
+        }
+
+        private void CheckLetter(object sender, RoutedEventArgs e)
+        {
+            if (txtBox.Text != "")
+            {
+                var letter = char.ToLower(txtBox.Text[0]);
+                bool succes = false;
+                for (int i = 0; i < pass.Length; i++)
+                {
+                    if (letter == pass[i] && currentlyPass[i] != letter)
+                    {
+                        currentlyPass[i] = letter;
+                        succes = true;
+                    }
+                }
+
+                if (!succes)
+                {
+                    //stickman refresh mistake
+                }
+
+                //call to refresh data
+                RefreshPasswordAndStickman();
+                txtBox.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("wpisz jakąś litere!", "Nie wpisano litery!");
+            }
         }
     }
 }
